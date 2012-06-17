d4effe7e3e3c57447009ca1f15937d0160f0b76a
Sergio Bossa <sergio.bossa@gmail.com>
2012-06-17 12:24:17 +0100

    Cosmetic refactorings.

--- src/main/java/journal/io/api/DataFileAccessor.java
+++ src/main/java/journal/io/api/DataFileAccessor.java
@@ -21,7 +21,6 @@ import java.util.Map.Entry;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
